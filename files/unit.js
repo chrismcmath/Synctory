888841ebec2c80c8a92c1376468dd61b8d6645f4
Chris McMath <chrismcmath@gmail.com>
2014-09-28 22:39:55 +0800

    Restyled unit buttons

--- files/unit.js
+++ files/unit.js
@@ -29,6 +29,7 @@ var Unit = new Class({
         if (this.Active) {
             this.CreateActive();
         } else {
+            this.CreateActive();
             this.CreateInactive();
         }
     },
@@ -40,30 +41,63 @@ var Unit = new Class({
         });
         this.TextDiv = new Element("textarea", {
             text: this.Text,
-            'class': 'unit_script'
+            'class': 'unit_script unit_activated'
         });
+        var header = new Element("div", {
+            'class': 'unit_header unit_activated'
+        });
+        var headerDivide = new Element("div", {
+            'class': 'unit_divide unit_divide_header'
+        });
+        var splitButton = new Element("div", {
+            html: '&#247;',
+            'class': 'split_unit unit_button'
+        });
+        var closeButton = new Element("div", {
+            html: '&#9679;',
+            'class': 'close_unit unit_button'
+        });
+        header.adopt(headerDivide);
+        header.adopt(splitButton);
+        header.adopt(closeButton);
+        var footer = new Element("div", {
+            'class': 'unit_footer unit_activated'
+        });
+        var footerDivide = new Element("div", {
+            'class': 'unit_divide unit_divide_footer'
+        });
+        var shrinkButton = new Element("div", {
+            html: '+',
+            'class': 'shrink_unit unit_button'
+        });
+        var growButton = new Element("div", {
+            html: '-',
+            'class': 'grow_unit unit_button'
+        });
+        footer.adopt(footerDivide);
+        footer.adopt(shrinkButton);
+        footer.adopt(growButton);
+        this.Div.adopt(header);
         this.Div.adopt(this.TextDiv);
+        this.Div.adopt(footer);
         $(GetLocationIDFromKey(this.LocationKey)).adopt(this.Div);
     },
 
     CreateInactive: function() {
-        this.Div = new Element("div", {
-            'class': 'unit unit_inactive',
-        'id': 'unit_' + this.Key
+        Array.each(this.Div.getElements('.unit_activated'), function (child, index) {
+            child.set({
+                styles: {
+                    display: 'none'
+                }
+            });
         });
+
         var plusDiv = new Element("div", {
             html: '+',
             'class': 'add_unit'
         });
-        this.TextDiv = new Element("textarea", {
-            styles: {
-                display: 'none'
-            },
-            'class': 'unit_script'
-        });
-        this.Div.adopt(this.TextDiv);
         this.Div.adopt(plusDiv);
-        $(GetLocationIDFromKey(this.LocationKey)).adopt(this.Div);
+        this.Div.addClass('unit_inactive');
 
         var unit = this;
         this.Div.addEvent('click', function(event){
@@ -78,13 +112,17 @@ var Unit = new Class({
         this.Active = true;
         this.Text = "NEW UNIT\nNEW UNIT\nNEW UNIT\nNEW UNIT\nNEW UNIT\nNEW UNIT\n";
         this.Entities = "";
-        this.Div.empty();
-        this.Div.setProperty('class', 'unit clear_all');
-        this.TextDiv = new Element("textarea", {
-            text: this.Text,
-            'class': 'unit_script'
+        this.Div.getElements('.add_unit').dispose();
+        Array.each(this.Div.getElements('.unit_activated'), function (child, index) {
+            child.set({
+                styles: {
+                    display: 'block'
+                }
+            });
         });
-        this.Div.adopt(this.TextDiv);
-        jQuery(this.TextDiv).elastic();
+        this.Div.removeClass('unit_inactive');
+        //this.Div.setProperty('class', 'unit clear_all');
+        this.TextDiv.value = this.Text;
+        //jQuery(this.TextDiv).elastic();
     }
 });
